2d5e7378537007cac171236058ae2a2637343906
tanya <tlathion@icloud.com>
2023-12-13 13:03:35 +0100

    :video_game: Creating controllers for each page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'contact/index'
+  get 'projects/index'
+  get 'projects/show'
+  get 'about/index'
+  get 'home/index'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
