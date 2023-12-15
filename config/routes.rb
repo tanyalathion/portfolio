83f3d39c8acacb82fe71c755a3f38eebda735f11
tanya <tlathion@icloud.com>
2023-12-15 17:25:19 +0100

    :bricks: Building my home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/home', to: 'home#index', as: 'home'
   get '/about', to: 'about#index'
   get '/projects', to: 'projects#index'
   get '/projects/:id', to: 'projects#show', as: 'project'
